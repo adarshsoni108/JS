223e29c0896e8a6f9fd0f85e5bfce2b7f2e774fa
unknown <adarshfulltime@gmail.com>
2022-09-28 22:32:00 +0530

    Adding comments in basics 03

--- basics-03-variables-operators/assets/scripts/app.js
+++ basics-03-variables-operators/assets/scripts/app.js
@@ -1,5 +1,19 @@
-const defaultResult = 0;
-let currentResult = defaultResult;
+// your Javascript code gets executed from top to bottom,
+
+// actually your HTML file gets parsed from top to bottom by the browser
+
+// and if you then import some Javascript in there, that file gets evaluated and executed by the browser
+
+// and the code in there also gets executed from top to bottom and
+
+// that's the default Javascript behavior and hence of course if you do something in this line and then you
+
+// use it in the line thereafter, you get the result from this line and not the result from the first line.
+
+// It would be pretty bad if that would be the case because then you could never do anything in your code.
+
+const defaultResult = 0; //value in here will never change
+let currentResult = defaultResult; //intialized the variable
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
